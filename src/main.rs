61707ad7c22dfa804ff9c6bdf1dd189407ba8627
Atul Bhatt <atulbhatt61@gmail.com>
2023-05-04 19:07:01 +0530

    Cell Added.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,26 @@ const SCREEN_SIZE: (f32, f32) = (
 
 const FPS: u32 = 120;
 
+#[derive(Clone, Debug)]
+struct Cell {
+    alive: bool,
+}
+
+impl Cell {
+    fn new(alive: bool) -> Cell {
+        Self { alive }
+    }
+
+    fn is_alive(&self) -> bool {
+        self.alive
+    }
+
+    fn set_state(&mut self, state: bool) {
+        self.alive = state;
+    }
+}
+
+#[derive(Copy, Clone)]
 struct Grid {
     pub x: i32,
     pub y: i32,
@@ -19,16 +39,26 @@ impl Grid {
     fn update(&mut self) {
         loop { }
     }
+
+    fn draw(self, canvas: &mut graphics::Canvas) {
+        canvas.draw(
+            &graphics::Quad,
+            graphics::DrawParam::new()
+                .dest_rect(graphics::Rect::new(1., 1., 150., 150.))
+                .color([0., 200., 0., 1.])
+        );
+    }
 }
 
-struct InitialState {
+struct MainState {
     grid: Grid
 }
 
-impl event::EventHandler for InitialState {
+impl event::EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
         while ctx.time.check_update_time(FPS) {
             self.grid.update();
+            self.draw(ctx)?;
         }
         Ok(())
     }
@@ -39,15 +69,17 @@ impl event::EventHandler for InitialState {
             ctx, graphics::Color::from([0.0, 1.0, 0.0, 1.0])
         );
 
+        self.grid.draw(&mut canvas);
+
         canvas.finish(ctx)?;
 
         Ok(())
     }
 }
 
-impl InitialState {
-    fn new() -> InitialState {
-        InitialState { grid: Grid { x:0, y:0 }}
+impl MainState {
+    fn new() -> MainState {
+        MainState { grid: Grid { x:0, y:0 }}
     }
 }
 
@@ -58,5 +90,5 @@ fn main() -> GameResult {
         .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
         .build()?;
 
-    event::run(ctx, events_loop, InitialState::new())
+    event::run(ctx, events_loop, MainState::new())
 }
