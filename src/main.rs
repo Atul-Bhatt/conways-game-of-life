9a7607f881ce9ca620711b0c6bf7c1afbc61a47e
Atul Bhatt <atulbhatt61@gmail.com>
2023-05-08 12:14:45 +0530

    Different ggez version

--- src/main.rs
+++ src/main.rs
@@ -31,14 +31,14 @@ impl Cell {
 }
 
 struct MainState {
-    first_cell: na::Point2<f32>,
+    cell_pos: f32,
 }
 
 
 impl MainState {
     fn new(ctx: &mut Context) -> Self {
         MainState { 
-            first_cell: na::Point2::new(10., 10.),
+            cell_pos: 0.0,
         }
     }
 }
@@ -55,8 +55,8 @@ impl event::EventHandler for MainState {
 
 fn main() -> GameResult {
     let cb = ggez::ContextBuilder::new("Game_of_Life", "Atul");
-    let (mut ctx, event_loop) = cb.build()?;
-    let state = MainState::new(&mut ctx);
-    event::run(ctx, event_loop, state);
-    Ok(())
+    let (mut ctx, mut event_loop) = cb.build()?;
+    graphics::set_window_title(&ctx, "Conway's Game of Life.");
+    let mut state = MainState::new(&mut ctx);
+    event::run(&mut ctx, &mut event_loop, &mut state)
 }
